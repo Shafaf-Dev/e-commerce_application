5919101136f94a17fc5aa3543ee0fad66d7104b4
shafafmuhammad6@gmail.com <shafafmuhammad6@gmail.com>
2021-05-16 16:38:08 +0530

    first commit

---
+++ ecommerce/store/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+
+def store(request):
+    context = {}
+    return render(request, 'store/store.html', context)
+
+
+def cart(request):
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+
+def checkout(request):
+    context = {}
+    return render(request, 'store/checkout.html', context)
