5bd4871f2c254ba8daa9baee2dcb5d09a6000480
Muhammed <shafafmuhammad6@gmail.com>
2021-05-27 03:14:57 +0530

    cart and order summery is dynamic

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -7,7 +7,6 @@ from .models import *
 
 
 
-
 def store(request):
     products = Product.objects.all()
     context = {'products':products}
@@ -15,10 +14,25 @@ def store(request):
 
 
 def cart(request):
-    context = {}
+
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+    context = {'items': items, 'order':order}
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'order': order}
     return render(request, 'store/checkout.html', context)
